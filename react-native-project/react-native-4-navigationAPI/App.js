8870380791f8fc1ab76e4985467397ef9cde1a69
dnjsxoghd <feelchok1234@gmail.com>
2024-04-16 18:22:24 +0900

    header setting

--- react-native-project/react-native-4-navigationAPI/App.js
+++ react-native-project/react-native-4-navigationAPI/App.js
@@ -1,24 +1,39 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import MealsOverviewScreen from './screens/MealsOverviewScreen'
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 
 import CategoriesScreens from "./screens/CategoriesScreen";
 
 const Stack = createNativeStackNavigator();
 
-
 export default function App() {
   return (
     <>
-    <StatusBar style="dark"></StatusBar>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="MealsCategories" component={CategoriesScreens}></Stack.Screen>
-        <Stack.Screen name="MealsOverView" component={MealsOverviewScreen}></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+      <StatusBar style="light"></StatusBar>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerTitleAlign: "center",
+            headerStyle: { backgroundColor: "#351401" },
+            headerTintColor: "white",
+            contentStyle: { backgroundColor: "#3f2f25" },
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreens}
+            options={{
+              title: "All Category",
+            }}
+          ></Stack.Screen>
+          <Stack.Screen
+            name="MealsOverView"
+            component={MealsOverviewScreen}
+          ></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
--- react-native-project/react-native-4-navigationAPI/screens/MealsOverviewScreen.js
+++ react-native-project/react-native-4-navigationAPI/screens/MealsOverviewScreen.js
@@ -1,22 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native';
-import {useRoute} from '@react-navigation/native'
+import { StyleSheet, FlatList, View } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
-function MealsOverviewScreen({route}){
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
 
-    const catId = route.params.categoryId;
+  const displayedMeals = MEALS.filter((item, index, arr) => {
+    return item.categoryIds.indexOf(catId) >= 0;
+  });
 
-    return(
-        <View style={styles.container}>
-            <Text>Meals Overview Screen - {catId}</Text>
-        </View>
-    )
+  function renderMealItem(itemData) {
+    const summaryData = itemData.item;
+
+    const data = {
+      title: summaryData.title,
+      imageUrl: summaryData.imageUrl,
+      affordability: summaryData.affordability,
+      complexity: summaryData.complexity,
+      duration: summaryData.duration,
+    };
+
+    return (
+      <MealItem
+        title={data.title}
+        imageURL={data.imageUrl}
+        affordability={data.affordability}
+        duration={data.duration}
+        complexity={data.complexity}
+      ></MealItem>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => {
+          return item.id;
+        }}
+        renderItem={renderMealItem}
+      ></FlatList>
+    </View>
+  );
 }
 
 export default MealsOverviewScreen;
 
 const styles = StyleSheet.create({
-    container : {
-        flex : 1,
-        padding : 16
-    }
-})
+
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+
+});
